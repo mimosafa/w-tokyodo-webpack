1f243a4f2751a5f4a4933784cb56a8ef4036791f
mimosafa <mimosafa@gmail.com>
2020-06-10 13:37:55 +0900

    add sass rule and reset.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const mode = 'development';
+
 const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: mode,
 
   entry: './src/index.js',
 
@@ -10,6 +13,25 @@ module.exports = {
     filename: 'bundle.js',
     path: outputPath
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
 
   devServer: {
     contentBase: outputPath,
